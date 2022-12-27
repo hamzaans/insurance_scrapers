be2d089dfa69951d50528274f20e6d2a55851b8f
hamzaans <hamza.ansari123@gmail.com>
2022-12-27 13:24:41 -0500

    added try except to handle exceptions in prioritypartnersscrape.py

--- prioritypartnersscrape.py
+++ prioritypartnersscrape.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
@@ -9,22 +10,24 @@ while j < 10:
     URL = "https://pp.healthtrioconnect.com/public-app/consumer/provdir/search.page?distance=25&startRow={page}0&sortType=DISTANCE&provType=S&myZip=20602".format(page = j)
     driver.get(URL)
     time.sleep(3) 
-
-    i = 1
-    while i < 300:
-             
-        doctor_name = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[1]/h3/a'.format(row = i))
-        specialty = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[1]/span[1]'.format(row = i))
-        group = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[3]/div'.format(row = i))
-        address = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[3]/address/span[1]'.format(row = i))
-        phone = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[3]/address/span[2]'.format(row = i))
-        print(doctor_name.text)
-        print(specialty.text)
-        print(group.text)
-        print(address.text)
-        print(phone.text)
-        print()
-        i += 1
+    try:
+        i = 1
+        while i < 300:
+                
+            doctor_name = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[1]/h3/a'.format(row = i))
+            specialty = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[1]/span[1]'.format(row = i))
+            group = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[3]/div'.format(row = i))
+            address = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[3]/address/span[1]'.format(row = i))
+            phone = driver.find_element(By.XPATH, '/html/body/div[4]/div/form/div/div[1]/div[6]/table/tbody/tr[{row}]/td[3]/address/span[2]'.format(row = i))
+            print(doctor_name.text)
+            print(specialty.text)
+            print(group.text)
+            print(address.text)
+            print(phone.text)
+            print()
+            i += 1
+    except Exception:
+        pass    
     j += 1
     driver.close()
     
